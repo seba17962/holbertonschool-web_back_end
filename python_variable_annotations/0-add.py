a8ef898a421838a2ecd5d023b8e4758aebbb2952
seba17962 <isebsebastiansilva@gmail.com>
2024-01-02 14:58:01 +0000

    task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -5,4 +5,13 @@
 
 
 def add(a: float, b: float) -> float:
+    """_summary_
+
+    Args:
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+        float: _description_
+    """
     return float(a + b)
