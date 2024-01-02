94fd5dcaa57b868b251b90ae787f33496a4610ed
seba17962 <isebsebastiansilva@gmail.com>
2024-01-02 15:18:07 +0000

    task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+
+
+def to_str(n: float) -> str:
+    """_summary_
+
+    Args:
+        n (float): _description_
+
+    Returns:
+        str: _description_
+    """
+    return str(n)
