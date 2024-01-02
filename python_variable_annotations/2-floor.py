cff009c52a1b8d924df7129ed1ebad4804df435b
seba17962 <isebsebastiansilva@gmail.com>
2024-01-02 15:15:57 +0000

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+
+def floor(n: float) -> int:
+    """_summary_
+
+    Args:
+        n (float): _description_
+
+    Returns:
+        int: _description_
+    """
+    return int(n)
