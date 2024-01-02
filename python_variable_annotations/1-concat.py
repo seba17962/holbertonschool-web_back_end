314e317e95ea45ae789c77f8c61d2782e7ecf9a1
seba17962 <isebsebastiansilva@gmail.com>
2024-01-02 15:12:47 +0000

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """_summary_
+
+    Args:
+        str1 (str): _description_
+        str2 (str): _description_
+
+    Returns:
+        str: _description_
+    """
+    return str(str1 + str2)
