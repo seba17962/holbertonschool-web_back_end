8604b50c89b3529c8a314a80401a3c8c465e69bf
seba17962 <isebsebastiansilva@gmail.com>
2024-01-03 13:51:43 +0000

    task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+from typing import Union, List
+
+list_type = Union[int, float]
+def sum_mixed_list(mxd_lst: List[list_type]) ->float:
+    """_summary_
+
+    Args:
+        mxd_lst (List[int, float]): _description_
+    """
+    add: float = sum(mxd_lst)
+    return add
