744839987fa9ac344234242dd353919f2097cb3e
seba17962 <isebsebastiansilva@gmail.com>
2024-01-03 14:28:48 +0000

    add dockstring

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,4 +5,12 @@ from typing import Iterable, List, Tuple, Sequence
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """_summary_
+
+    Args:
+        lst (Iterable[Sequence]): _description_
+
+    Returns:
+        List[Tuple[Sequence, int]]: _description_
+    """
     return [(i, len(i)) for i in lst]
