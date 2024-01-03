b0c519ee80696533fb421f519ee960714648d3cd
seba17962 <isebsebastiansilva@gmail.com>
2024-01-03 19:53:46 +0000

    task 0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" doc"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """_summary_
+
+    Args:
+        max_delay (int, optional): _description_. Defaults to 10.
+
+    Returns:
+        float: _description_
+    """
+    delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
