f061d043b5ad2d80f24f52e3efaa00e7175fd64c
seba17962 <isebsebastiansilva@gmail.com>
2024-01-03 20:21:54 +0000

    task 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """_summary_
+
+    Args:
+        n (int): _description_
+        max_delay (int): _description_
+
+    Returns:
+        List[float]: _description_
+    """
+    dictionary = []
+    for _ in range(0, n):
+        delay = await wait_random(max_delay)
+        dictionary.append(delay)
+    dictionary.sort()
