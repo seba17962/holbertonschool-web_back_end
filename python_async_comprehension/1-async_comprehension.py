24a5556708f5d63279e4d970a7c44c225aa4b302
seba17962 <isebsebastiansilva@gmail.com>
2024-01-04 14:53:13 +0000

    task 1

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    random_num = async_generator
+    return random_num
