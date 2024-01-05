6d4dfebdea8e2c52a850ad8bf259b0f8feb3076e
seba17962 <isebsebastiansilva@gmail.com>
2024-01-05 14:13:30 +0000

    add anotations, and change the import

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,15 +2,15 @@
 """_summary_
 """
 import asyncio
-from random import uniform
+import random
 
 
-async def async_generator():
+async def async_generator() -> float:
     """_summary_
 
     Yields:
         _type_: _description_
     """
-    for _ in range(0, 10):
+    for _ in range(10):
         await asyncio.sleep(1)
-        yield uniform(0, 10)
+        yield random.uniform(0, 10)
