412187de8b33b81bc381895d34dfb6451c15d027
seba17962 <isebsebastiansilva@gmail.com>
2024-01-05 14:18:45 +0000

    task 2

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 """_summary_
 """
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """_summary_
 
     Returns:
-        _type_: _description_
+        List[float]: _description_
     """
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    return result
+    res = [i async for i in async_generator()]
+    return res
