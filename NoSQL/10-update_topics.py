f669819929e38c804fd28a1a283b3d1f9f94682a
seba17962 <isebsebastiansilva@gmail.com>
2024-01-10 14:20:56 +0000

    task 10

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """_summary_
+
+    Args:
+        mongo_collection (_type_): _description_
+        name (_type_): _description_
+        topics (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    if mongo_collection is not None:
+        return mongo_collection.update({ "name": name },
+                                       { set: { "topics": topics } },
+                                       { multi: true }
+                                       )
