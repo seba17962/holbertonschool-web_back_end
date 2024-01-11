6df856461d98649ac7c732ea1f680d7cfee4e1f6
seba17962 <isebsebastiansilva@gmail.com>
2024-01-11 15:00:51 +0000

    task 0

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+
+
+def index_range(page, page_size):
+    """_summary_
+
+    Args:
+        page (_type_): _description_
+        page_size (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    tuple = (page, page_size)
+    return tuple
