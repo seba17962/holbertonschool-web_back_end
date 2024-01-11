4f9b10d16f5e050fdbf8ee848038cd29a5935e71
seba17962 <isebsebastiansilva@gmail.com>
2024-01-11 20:39:09 +0000

    add get_hyper that returns a dictionary

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -65,4 +65,21 @@ class Server:
             return dataset[start_index:end_index]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        
+        data = self.get_page(page, page_size)
+        if self.get_page(page + 1, page_size):
+            next_page = page + 1
+        else:
+            next_page = None
+            total_pages = page
+        if self.get_page(page - 1, page_size):
+            prev_page = page + 1
+        else:
+            prev_page = None
+        dict= {
+            "page_size": page_size,
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages
+        }
