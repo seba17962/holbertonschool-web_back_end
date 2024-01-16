8c5397d21968881804df8e13261b64fd7101743d
seba17962 <isebsebastiansilva@gmail.com>
2024-01-16 14:44:43 +0000

    task 4

--- ES6_basic/4-rest-parameter.js
+++ ES6_basic/4-rest-parameter.js
@@ -1,9 +1,7 @@
 export default function returnHowManyArguments(...args) {
   let total = 0;
-  if (args != null) {
-    for (const arg in args){
-      total += 1;
-    }
+  for (const _ in args){
+    total += 1;
   }
   return total
 }
