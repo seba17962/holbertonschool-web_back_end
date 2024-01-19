901737548a61ca85b292e6511812637e6ff28e7c
seba17962 <isebsebastiansilva@gmail.com>
2024-01-19 15:07:17 +0000

    task 1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,7 @@
-import ClassRoom from "./0-classroom.js";
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   const roomsSizes = [19, 20, 34];
-  const rooms = roomsSizes.map(size => new ClassRoom(size));
+  const rooms = roomsSizes.map((size) => new ClassRoom(size));
   return rooms;
 }
