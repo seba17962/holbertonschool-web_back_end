46b44643fbcb49872f255e2cb4cfd169aebf2f87
seba17962 <isebsebastiansilva@gmail.com>
2024-01-19 13:36:57 +0000

    task 9

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,7 +1,7 @@
 import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
-  const budget = getBudgetObject(income, gdp, capita);
+  const budget = getBudgetObject();
   const fullBudget = {
     ...budget,
     getIncomeInDollars: function () {
