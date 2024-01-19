e7762445e0a309a1037e6e178918cee62860225b
seba17962 <isebsebastiansilva@gmail.com>
2024-01-19 14:06:06 +0000

    task 10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (value of array) {
-    const value = appendString + value;
+  for (let value of array) {
+    value = appendString + value;
   }
 
   return array;
