f7cf50218162b6313920a421207e911f8ed9b61e
seba17962 <isebsebastiansilva@gmail.com>
2024-01-21 15:13:45 +0000

    task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,25 +1,13 @@
 export default class Building {
   constructor (sqft) {
-    if (typeof (sqft) === 'number') {
-      this._sqft = sqft;
-    } else {
-      throw new TypeError('sqft must be a number');
+    this._sqft = sqft;
+    if (
+      this.constructor !== Building
+      && !this.evacuationWarningMessage
+    ) throw new Error('Class extending Building must override evacuationWarningMessage');
     }
-  }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(value) {
-    if (typeof (value) === 'number') {
-      this._sqft = value;
-    } else {
-      throw new TypeError('sqft must be a number');
-    }
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
