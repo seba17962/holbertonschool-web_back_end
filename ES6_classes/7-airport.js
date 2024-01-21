b4e8284216848e6bf5704296b0b7029f41f7e592
seba17962 <isebsebastiansilva@gmail.com>
2024-01-21 20:01:30 +0000

    task 7

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,9 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+  toString() {
+    return `Airport [${this._code}] { _name: '${this._name}', _code: '${_code}'}`
+  }
+}
