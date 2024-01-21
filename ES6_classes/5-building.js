f05945cb0820723c9c46d9db921947cc85b2227c
seba17962 <isebsebastiansilva@gmail.com>
2024-01-21 15:15:42 +0000

    task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,11 +1,11 @@
 export default class Building {
-  constructor (sqft) {
+  constructor(sqft) {
     this._sqft = sqft;
     if (
       this.constructor !== Building
       && !this.evacuationWarningMessage
     ) throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+  }
 
   get sqft() {
     return this._sqft;
