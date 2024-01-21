4b41ae0060a6366142ad2d0a15d001506f744064
seba17962 <isebsebastiansilva@gmail.com>
2024-01-21 15:27:38 +0000

    task 6

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -14,7 +14,7 @@ export default class SkyHighBuilding extends Building {
     return this._floors;
   }
 
-  evacuationWarningMessage(){
-    return `Evacuate slowly the ${this._floors} floors`
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
