f6e4028a654664a778168289a047525890f0a554
seba17962 <isebsebastiansilva@gmail.com>
2024-01-27 15:18:37 +0000

    task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -7,6 +7,7 @@ export default function handleProfileSignup() {
   Promise.all([photoPromise, userPromise])
     .then(([photoResult, userResult]) => {
       console.log(`${photoResult.body} ${userResult.firstName} ${userResult.lastName}`);
+      return `${photoResult.body} ${userResult.firstName} ${userResult.lastName}`
     })
     .catch((error) => {
       console.log('Signup system offline');
