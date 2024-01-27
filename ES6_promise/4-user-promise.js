be343b1083768f63403a53e908230dec75b6b83d
seba17962 <isebsebastiansilva@gmail.com>
2024-01-27 14:50:52 +0000

    task 4

---
+++ ES6_promise/4-user-promise.js
@@ -0,0 +1,7 @@
+export default function signUpUser(firstName, lastName) {
+  return new Promise((resolve, reject) => {
+    resolve({ firstName: firstName, lastName: lastName });
+
+    reject(new Error());
+  });
+}
