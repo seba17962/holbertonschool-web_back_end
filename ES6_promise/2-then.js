4f89aca8d35714c957929351fe53f0bfb78223e9
seba17962 <isebsebastiansilva@gmail.com>
2024-01-27 13:33:07 +0000

    task 2

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   promise
     .then(() => {
-      return { status: 200, body: 'success' };
+      return { body: 'success', status: 200 };
     })
     .catch(() => {
       return new Error()
