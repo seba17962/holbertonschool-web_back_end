84cc2ea13e525cc99ab5cacf4950300715628770
seba17962 <isebsebastiansilva@gmail.com>
2024-01-29 13:59:49 +0000

    task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,10 +1,13 @@
-import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    uploadPhoto(fileName),
-    signUpUser(firstName, lastName)
-  ])
-    .then((result) => (result));
+  const promise1 = signUpUser(firstName, lastName, fileName);
+  const promise2 = uploadPhoto(fileName);
+
+  return Promise.allSettled([promise1, promise2])
+    .then((results) => results.map((res) => ({
+      status: res.status,
+      value: res.value || res.reason.toString(),
+  })));
 }
