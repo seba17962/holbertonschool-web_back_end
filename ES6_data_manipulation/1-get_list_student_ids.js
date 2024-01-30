fedb9f85df4570b90a60f1842b35ef9148822cf7
seba17962 <isebsebastiansilva@gmail.com>
2024-01-30 13:30:19 +0000

    task 1

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,7 @@
 export default function getListStudentIds(array) {
+  if (!Array.isArray(array)) {
+    return [];
+  }
   const ids = array.map(function(studentsId) {
     return studentsId.id
   });
