a3f14e01f1c858cf194a29447ca36beef5c1727b
seba17962 <isebsebastiansilva@gmail.com>
2024-01-30 14:30:56 +0000

    task 3

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -2,10 +2,10 @@ export default function getStudentIdsSum(studentsList) {
   if (!Array.isArray(studentsList)) {
     return [];
   }
-
+  /* eslint-disable arrow-body-style */
   const idSum = studentsList.reduce((accumulator, students) => {
     return accumulator + students.id;
   }, 0);
-
+  /* eslint-enable arrow-body-style */
   return idSum;
 }
