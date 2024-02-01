7de29d457ef01252e1103d6650ca38b19f1c4ea4
seba17962 <isebsebastiansilva@gmail.com>
2024-02-01 14:49:02 +0000

    task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
   if (!startString || !startString.length) return '';
   return Array.from(set)
-  .filter(value => value.startsWith(startString))
-  .map(value => {
-    return value.slice(startString.length)
-  })
-  .join('-');
+    .filter((value) => value.startsWith(startString))
+    .map((value) => {
+      return value.slice(startString.length);
+   })
+   .join('-');
 }
