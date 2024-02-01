fe6e916b89416df26fe75b2717004b36741cc07b
seba17962 <isebsebastiansilva@gmail.com>
2024-02-01 13:42:17 +0000

    task 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,15 @@
 export default function updateStudentGradeByCity(studentsList, city, newGrades) {
   const filteredStudents = studentsList.filter((student) => student.location === city);
 
-  const updatedStudents = filteredStudents.map(student => {
-  const newGrade = newGrades.find(grade => grade.studentId === student.id);
+  const updatedStudents = filteredStudents.map((student) => {
+    const newGrade = newGrades.find(grade => grade.studentId === student.id);
 
-  const grade = newGrade && newGrade.grade || 'N/A';
+    const grade = newGrade ? newGrade.grade : 'N/A';
 
-
-  return {
-    ...student,
-    grade: grade,
-  };
-});
+    return {
+      ...student,
+      grade: grade,
+    };
+  });
   return updatedStudents;
 }
